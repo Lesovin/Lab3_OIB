e68987dc251b60780b524fa5cf3a584248ff116b
Стефан Степанов <a1806430@yandex.ru>
2021-11-23 12:42:36 +0400

    Создал репозиторий

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
+from cryptography.hazmat.primitives import hashes
+import os
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required = True)
+group.add_argument('-gen','--generation',help='Запускает режим генерации ключей')
+group.add_argument('-enc','--encryption',help='Запускает режим шифрования')
+group.add_argument('-dec','--decryption',help='Запускает режим дешифрования')
+
+args = parser.parse_args()
+if args.generation is not None:
+  # генерируем ключи
+else if args.encryption is not None:
+  # шифруем
+else:
